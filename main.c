10a0ed0b1ab16309c95a212475f2a79114f58863
SAR41k <92851936+SAR41k@users.noreply.github.com>
2021-11-22 12:44:02 +0400

    main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int yearbirds;
+int currentyear;
+int age;
+yearbirds=1980;
+currentyear=2010;
+age=currentyear-yearbirds;
+printf("Age of student: %d years old. \n", age);
+return 0;
+}
